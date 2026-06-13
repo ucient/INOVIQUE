30c18dec2ac01b768da5c57f211e086d097cf00a
ucient <parthmakwana1557@gmail.com>
2026-06-13 16:17:20 +0530

    first UI commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 const App = () => {
   return (
     <main className="w-screen h-screen bg-black">
-      <h1 className="text-white">app</h1>
+      <h1 className="text-white">start your UI code from here akshat</h1>
     </main>
   );
 };
