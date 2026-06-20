66d397d258c623f0853bdad86e22f95f6c7ef29d
ucient <parthmakwana1557@gmail.com>
2026-06-20 12:46:53 +0530

    all things set

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,13 @@ import express from "express";
 import routes from "./src/modules/index.js";
 import { connectDB } from "./src/config/db.config.js";
 import { errorHandler } from "./src/shared/middlewares/error.middleware.js";
-// import { rateLimiter } from "./src/shared/middlewares/rateLimit.middleware.js";
-import rateLimiter from "./src/shared/middlewares/rateLimit.middleware.js";
-console.log(rateLimiter)
+import { rateLimiter } from "./src/shared/middlewares/rateLimit.middleware.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
-// app.use(rateLimiter());
+app.use(rateLimiter());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
